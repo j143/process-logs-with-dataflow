0a2b68e0493da81b8a459a243ec3c7e9946ee47e
Janardhan Pulivarthi <j143@protonmail.com>
2021-07-07 03:52:55 +0000

    add PCollection and imports

--- src/main/java/page/janardhan/labs/App.java
+++ src/main/java/page/janardhan/labs/App.java
@@ -1,5 +1,11 @@
 package page.janardhan.labs;
 
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.io.TextIO;
+
 /**
  * Hello world!
  *
@@ -10,4 +16,12 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    PipelineOptions options = PipelineOptionsFactory.create();
+    Pipeline p = Pipeline.create(options);
+
+    PCollection<String> homeLogs = p.apply("homeLogsTextRead", TextIO.read().from(options.getHomeLogSource()));
+    PCollection<String> browseLogs = p.apply("browseLogsTextRead", TextIO.read().from(options.getBrowseLogSource()));
+    // PCollection<String> 
+    
 }
